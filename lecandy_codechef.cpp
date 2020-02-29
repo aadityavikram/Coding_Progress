a2204b1e1690ceb00373711e9ddc37da8701bdf8
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2020-02-29 17:06:15 +0530

    Create lecandy_codechef.cpp

---
+++ lecandy_codechef.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void fastIO(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void process(int arr[],int n,int c){
+    int sum=0;
+    for(int i=0;i<n;i++) sum+=arr[i];
+    if(sum>c) cout<<"No\n";
+    else cout<<"Yes\n";
+}
+
+void start(){
+    int c,n,t;
+    cin>>t;
+    while(t--){
+        cin>>n>>c;
+        int arr[n];
+        for(int i=0;i<n;i++) cin>>arr[i];
+        process(arr,n,c);
+    }
+}
+
+int main() {
+	fastIO();
+	start();
+	return 0;
+}
