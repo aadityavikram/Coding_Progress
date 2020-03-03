3d5b23a73073ab49a83138f9f27e6ec20fa8583d
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2020-03-03 10:54:38 +0530

    Update bigcountry.sql

--- bigcountry.sql
+++ bigcountry.sql
@@ -1,3 +1,3 @@
-// https://leetcode.com/problems/big-countries/
+-- https://leetcode.com/problems/big-countries/
 
 SELECT name, population, area from World where area > 3000000 or population > 25000000
