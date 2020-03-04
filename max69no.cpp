ad51690fe3a16fe5ad801ff32e354b4459e08797
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2020-03-04 14:45:45 +0530

    Update max69no.cpp

--- max69no.cpp
+++ max69no.cpp
@@ -1,7 +1,6 @@
 // https://leetcode.com/problems/maximum-69-number/
 
 int maximum69Number (int num) {
-        int flag=1;
         string n=to_string(num);
         for(int i=0;i<n.length();i++)
             if(n[i]=='6'){
