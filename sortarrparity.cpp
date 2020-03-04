928f115058801994d2d83a96b92d2bc346c97be5
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2020-03-04 11:15:45 +0530

    Update sortarrparity.cpp

--- sortarrparity.cpp
+++ sortarrparity.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/sort-array-by-parity/
 
+// Solution 1: -
 vector<int> sortArrayByParity(vector<int>& A) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
@@ -10,4 +11,15 @@ vector<int> sortArrayByParity(vector<int>& A) {
             if(num%2==1) resOdd.push_back(num);
         }resEven.insert(resEven.end(), resOdd.begin(), resOdd.end());
         return resEven;
+    }
+
+// Solution 2: -
+vector<int> sortArrayByParity(vector<int>& A) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        partition(begin(A),end(A),[](int a){
+            return a%2==0;
+        });
+        return A;
     }
