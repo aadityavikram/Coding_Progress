9349bca7f5d0daba36684f928aae5170611a63a7
Aaditya Vikram <31779873+aadityavikram@users.noreply.github.com>
2020-03-05 20:15:03 +0530

    Create keyboardrow.cpp

---
+++ keyboardrow.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/keyboard-row/
+
+void fastIO(){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+    vector<string> findWords(vector<string>& words) {
+        fastIO();
+        vector<string> vec;
+        int hash[26]={2,3,3,2,1,2,2,2,1,2,2,2,3,3,1,1,1,1,2,1,1,3,1,3,1,3};
+        for(string word:words){
+            int temp=(word[0]>='A'&&word[0]<='Z')?hash[word[0]-'A']:hash[word[0]-'a'];
+            int flag=1;
+            for(char letter:word)
+                if((letter>='A'&&letter<='Z'&&hash[letter-'A']!=temp)||
+                   (letter>='a'&&letter<='z'&&hash[letter-'a']!=temp)){
+                        flag=0;
+                        break;
+                    }
+            if(flag==1) vec.push_back(word);
+        }return vec;
+    }
